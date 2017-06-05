c04115c79cd1e2bacc48af0a008c40d778d2e0f1
Bren Briggs <briggs.brenton@gmail.com>
2017-06-05 18:51:11 -0500

    Link sithmail-tipbot to redis container

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -d -it --net=host --name $1 -v "$PWD":/usr/src/legobot -w /usr/src/legobot legobot
+docker run -d -it --net=host --link sithmail-redis:redis --name $1 -v "$PWD":/usr/src/legobot -w /usr/src/legobot legobot
